95ab6089ae8b74a146a5c50cd156d4f1d94c2149
Forrest Hopkins <fhopkins404@gmail.com>
2022-03-18 18:26:24 -0700

    Initial commit

---
+++ scratch.py
@@ -0,0 +1,94 @@
+from subprocess import Popen, PIPE
+from datetime import datetime, timedelta
+from ytmusicapi import YTMusic
+import requests
+import urllib.parse
+import json
+import re
+
+def main():
+    now_playing = execute_script('''
+        tell application "System Events"
+            tell its application process "ControlCenter"
+                click (first menu bar item whose name is "Now Playing") of menu bar 1
+                set track to name of first static text of group 1 of first item of windows
+                key code 53
+                return track
+            end tell
+        end tell
+    ''')
+
+    print('now playing:')
+    print(now_playing)
+
+    ytmusic = YTMusic('ytm-auth.json')
+
+    latest_track = ytmusic.get_history()[0]['title']
+
+    print('latest track:')
+    print(latest_track)
+
+    if any(blocked.lower() in latest_track.lower() for blocked in get_blocklist()):
+        print("blocked")
+        quit()
+
+    if now_playing.startswith(latest_track):
+        print("Updating status")
+        update_status(latest_track, 'headphones')
+
+
+# def update_status(message, icon):
+#     expiry = datetime.now() + timedelta(minutes=5)
+#     expiry_str = expiry.strftime("%H:%M")
+#     execute_script(f'tell script "Slack" to set status "{message}" with icon ":{icon}:"')
+
+
+def execute_script(script):
+    process = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    stdout, stderr = process.communicate(script)
+    return stdout
+
+
+def get_blocklist():
+    with open('blocklist.txt') as file:
+        return [line.rstrip() for line in file]
+
+
+def urlencode(str):
+    return urllib.parse.quote_plus(str)
+
+
+def set_status(text, emoji, minutes):
+    xc, xd, xds = read_har()
+    expiry = round((datetime.now() + timedelta(minutes=minutes)).timestamp())
+    cookies = {
+        'd': urlencode(xd),
+        'd-s': xds
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    profile = {
+        "status_emoji": f':{emoji}:',
+        "status_expiration": expiry,
+        "status_text": text,
+        "status_text_canonical": ""
+    }
+    body = f'profile={urlencode(json.dumps(profile))}&token={xc}'
+    r = requests.post('https://amzn-aws.slack.com/api/users.profile.set', cookies=cookies, headers=headers, data=body)
+
+    return r.json()
+
+def read_har():
+    with open('slack-auth.har') as file:
+        data = json.load(file)
+    request = data['log']['entries'][0]['request']
+    postData = request['postData']['text']
+    xc = re.search("xoxc-[^\\r]*", postData).group()
+    cookies = request['cookies']
+    xd = next(c['value'] for c in cookies if c['name'] == 'd')
+    xds = next(c['value'] for c in cookies if c['name'] == 'd-s')
+    return xc, xd, xds
+
+
+set_status('weekend', 'catjam', 30)
